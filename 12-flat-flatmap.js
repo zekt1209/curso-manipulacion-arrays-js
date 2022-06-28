192c03e44a515b31f5eb8d61d756aa15032b695b
Soporte Sistemas 3 <soporte.sistemas3@sto.com.mx>
2022-06-28 13:29:03 -0500

    Finalizacion del curso, se agregan archivos faltantes 12-flat-flatmap

--- 12-flat-flatmap.js
+++ 12-flat-flatmap.js
@@ -1,1 +1,17 @@
-// flatmap
+// flat - flatmap
+
+/*
+flatMap() es un método que primero mapea cada elemento, 
+y después aplana el resultado en un nuevo array.
+
+
+*/
+
+const users = [
+    { userId: 1, username: "Tom", attributes: ["Nice", "Cute"]},
+    { userId: 2, username: "Mike", attributes: ["Lovely"]},
+    { userId: 3, username: "Nico", attributes: ["Nice", "Cool"]},
+  ]
+  
+  const arrayFlatted = users.flat();
+  
